40c8255e8ae05270f4a4c90dceee6c17a46e9846
damletanmay <tanmay.damle.25@gmail.com>
2020-12-13 11:46:14 +0530

    Finalizing things, other projects of react on codesandbox.io

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,7 +1,10 @@
 import React,{useState} from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
-  
+  const [isExpanded,set]=useState(false);
   const [note,setNote] = useState({
     title:"",
     content:""
@@ -25,15 +28,30 @@ function handleClick(event){
   event.preventDefault();
 }
 
+function showTitle(){
+  set(true);
+}
+
   return (
     <div>
-      <form>
-        <input onChange={handleChange} name="title" placeholder="Title" value = {note.title} />
-        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value = {note.content} />
-        <button onClick={handleClick}>Add</button>
-      </form>
+      <form className="create-note">
+        <input  type={isExpanded ? "input" : "hidden" } onChange={handleChange} name="title" placeholder="Title" value = {note.title} /> 
+        <textarea onClick={showTitle}
+         onChange={handleChange} 
+         name="content" 
+         placeholder="Take a note..."
+         rows={isExpanded ? 3:1 }
+         value = {note.content} />   
+         <Zoom in = {isExpanded}>
+        <Fab onClick={handleClick}>
+        <AddIcon />
+        </Fab>
+        </Zoom> 
+         </form>
     </div>
   );
 }
 
 export default CreateArea;
+
+
